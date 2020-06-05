0a25b82796bf650f22aaf2f2284fcd6d7d82001b
Shmulik <29735690+Shmulik-Kravitz@users.noreply.github.com>
2020-06-05 09:30:24 +0300

    Add build system

---
+++ build/reactJewishDatePicker.d.ts
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import "./reactJewishDatePicker.scss";
+export interface ReactJewishDatePickerProps {
+}
+export declare const ReactJewishDatePicker: React.FC<any>;
---
+++ src/reactJewishDatePicker.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import "./reactJewishDatePicker.scss";
+
+export interface ReactJewishDatePickerProps {
+
+}
+
+export const ReactJewishDatePicker: React.FC<any> = (props: any) => {
+    return (
+        <div className={"reactJewishDatePicker"}>
+            Date Picker
+        </div>
+    )
+}
