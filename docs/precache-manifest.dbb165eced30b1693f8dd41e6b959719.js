b4169161e0d7f462d0165fd616f4141c3d8a1a5e
Shmulik <29735690+Shmulik-Kravitz@users.noreply.github.com>
2020-08-28 17:34:27 +0300

    add docs

---
+++ docs/precache-manifest.dbb165eced30b1693f8dd41e6b959719.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7362a1eb653e6803e30eddbcddf461e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d61f0376a5157ce50b8a",
+    "url": "/static/css/main.1751d893.chunk.css"
+  },
+  {
+    "revision": "e024d7dc6e36ab81617f",
+    "url": "/static/js/2.a36e7467.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a36e7467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d61f0376a5157ce50b8a",
+    "url": "/static/js/main.309b3e9e.chunk.js"
+  },
+  {
+    "revision": "31a8f28bc17566716807b83469015170",
+    "url": "/static/js/main.309b3e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc51d2a1e6b2c6d65a5b",
+    "url": "/static/js/runtime-main.1bd49777.js"
+  }
+]);
