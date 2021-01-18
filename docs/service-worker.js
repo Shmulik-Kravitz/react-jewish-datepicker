f5a7c0c97058ee8bcadd62ef60c1694cd02c2177
Shmulik <29735690+Shmulik-Kravitz@users.noreply.github.com>
2021-01-18 15:28:38 +0200

    v1.0.7

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c8279cd410adc0ea097f4c370ee43f64.js"
+  "/precache-manifest.50940bf6c228473b4d68964e8ddd8a18.js"
 );
 
 self.addEventListener('message', (event) => {
